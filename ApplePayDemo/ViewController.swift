289adfa6ed560fd1dc4ae44bf7d99fdcf74fe802
Vikash Kumar <vikash@vikashs-mac-mini.local>
2017-06-16 15:58:01 +0530

    - shipping methods added

--- ApplePayDemo/ViewController.swift
+++ ApplePayDemo/ViewController.swift
@@ -45,31 +45,56 @@ class ViewController: UIViewController {
     func createPaymentRequest()-> PKPaymentRequest {
         let request = PKPaymentRequest()
         request.merchantIdentifier = "merchant.com.applepay.vikash"
-        request.countryCode = "IN"
-        request.currencyCode = "INR"
+        request.countryCode = "US"
+        request.currencyCode = "USD"
         request.merchantCapabilities = .capability3DS
         request.paymentSummaryItems = paymentSummeryItems()
+        request.shippingMethods = shippingMethods()
         return request
     }
     
-    func paymentSummeryItems()->[PKPaymentSummaryItem] {
+    func paymentSummeryItems(shipAmount: Double = 0.0)->[PKPaymentSummaryItem] {
         let subtotal = PKPaymentSummaryItem(label: "Sub Total", amount: NSDecimalNumber(value: 20.0))
         let servTax = PKPaymentSummaryItem(label: "Tax", amount: NSDecimalNumber(value: -5))
         
-        let total = subtotal.amount.doubleValue + servTax.amount.doubleValue
-        let totalItem = PKPaymentSummaryItem(label: "Total Amounts", amount: NSDecimalNumber(value: total))
+        let shippingAmount = PKPaymentSummaryItem(label: "Shipping Charge", amount: NSDecimalNumber(value: shipAmount))
         
-        return [subtotal, servTax, totalItem]
+        let totalAmount = subtotal.amount.adding(servTax.amount).adding(shippingAmount.amount)
+        let total = PKPaymentSummaryItem(label: "Total Amounts", amount: totalAmount)
+        
+        return [subtotal, servTax, shippingAmount, total]
+    }
+    
+    func shippingMethods()-> [PKShippingMethod] {
+        let twoDaysDelivery = PKShippingMethod(label: "Super Fast Shipping.", amount: NSDecimalNumber(value: 15))
+        twoDaysDelivery.identifier = "s_fast_shipping"
+        twoDaysDelivery.detail = "Shipping withing 2 days"
+        
+        let normalDelevery = PKShippingMethod(label: "Fast Shipping", amount: NSDecimalNumber(value: 10))
+        normalDelevery.identifier = "fast_shipping"
+        normalDelevery.detail = "Shipping withing 4-5 days"
+
+        let free = PKShippingMethod(label: "Free Shipping", amount: NSDecimalNumber(value: 0))
+        free.identifier = "free_shipping"
+        free.detail = "Shipping withing 7-8 days"
+
+        return [free, twoDaysDelivery, normalDelevery]
     }
     
 }
 
 extension ViewController: PKPaymentAuthorizationViewControllerDelegate {
+    
+    func paymentAuthorizationViewController(_ controller: PKPaymentAuthorizationViewController, didAuthorizePayment payment: PKPayment, completion: @escaping (PKPaymentAuthorizationStatus) -> Void) {
+        completion(.failure)
+    }
+    
     func paymentAuthorizationViewControllerDidFinish(_ controller: PKPaymentAuthorizationViewController) {
         controller.dismiss(animated: true, completion: nil)
     }
-    func paymentAuthorizationViewController(_ controller: PKPaymentAuthorizationViewController, didAuthorizePayment payment: PKPayment, completion: @escaping (PKPaymentAuthorizationStatus) -> Void) {
-        completion(.success)
+
+    func paymentAuthorizationViewController(_ controller: PKPaymentAuthorizationViewController, didSelect shippingMethod: PKShippingMethod, completion: @escaping (PKPaymentAuthorizationStatus, [PKPaymentSummaryItem]) -> Void) {
+        completion(.success, paymentSummeryItems(shipAmount: Double(shippingMethod.amount)))
     }
 }
 
